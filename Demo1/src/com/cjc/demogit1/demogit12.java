94698c7cbb758d11e39fcfe0df66624ddab91653
USER <USER@DESKTOP-7CMDVD0>
2021-04-21 00:31:35 +0530

    this is my first project

---
+++ Demo1/src/com/cjc/demogit1/demogit12.java
@@ -0,0 +1,38 @@
+package com.cjc.demogit1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demogit12 
+{
+	WebDriver driver;
+	@org.testng.annotations.Test(priority=1)
+	public void Open_browser()
+	{
+		System.setProperty("webdriver.chrome.driver","E:\\exe\\chromedriver.exe" );
+	    driver=new ChromeDriver();
+		System.out.println("OPen the browser");
+		driver.get("http://demowebshop.tricentis.com/");
+		System.out.println("launch the url");
+	
+	}
+	@org.testng.annotations.Test(priority=2)
+	public void Test()
+	{  driver.findElement(By.xpath("//*[@class=\"ico-register\"]")).click();
+	    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	   driver.findElement(By.xpath("//*[@id=\"gender-female\"]")).click();
+	   driver.findElement(By.xpath("//*[@id=\"FirstName\"]")).sendKeys("shweta");
+	   driver.findElement(By.xpath("//*[@id=\"LastName\"]")).sendKeys("kale");
+	   driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("kale4shweta@gmail.com");
+	   driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("shweta29");
+	   driver.findElement(By.xpath("//*[@id=\"ConfirmPassword\"]")).sendKeys("shweta29");
+	   driver.findElement(By.xpath("//*[@id=\"register-button\"]")).click();
+	   System.out.println("Test");
+		
+	}
+	
+
+}
